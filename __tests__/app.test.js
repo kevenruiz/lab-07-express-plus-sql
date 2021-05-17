cf40f16088bc8b4b4731a0bb30ff6fc47acd8452
keven-ruiz <kevenruizh@gmail.com>
2021-05-17 00:01:00 -0700

    Loading test one, cannot get it to past.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,22 +7,27 @@ const request = supertest(app);
 
 describe('API Routes', () => {
 
-  beforeAll(() => {
+  /*beforeAll(() => {
     execSync('npm run recreate-tables');
   });
+  */
 
-  /*beforeAll(() => {
+  beforeAll(() => {
     execSync('npm run setup-db');
   });
-*/
+
 
   afterAll(async () => {
     return client.end();
   });
 
-  describe(('/api/pokemon CRUD'), () => {
+  //describe(('/api/pokemon CRUD'), () => {
 
-    let butterfree = {
+
+  //});
+  /*
+  const expectedPokemon = [
+    {
       'id': 1,
       'name': 'butterfree',
       'pokemon_number': 16,
@@ -30,9 +35,9 @@ describe('API Routes', () => {
       'ability_1': 'compound-eyes',
       'url_image': 'http://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png',
       'isMegaEvolution': false
-    };
+    },
 
-    let venasaur = {
+    {
       'id': 2,
       'name': 'venusaur-mega',
       'pokemon_number': 4,
@@ -40,9 +45,8 @@ describe('API Routes', () => {
       'ability_1': 'thick-fat',
       'url_image': 'http://assets.pokemon.com/assets/cms2/img/pokedex/full/003_f2.png',
       'isMegaEvolution': true
-    };
-
-    let pidgeotto = {
+    },
+    {
       'id': 3,
       'name': 'pidgeotto',
       'pokemon_number': 22,
@@ -50,8 +54,8 @@ describe('API Routes', () => {
       'ability_1': 'keen-eye',
       'url_image': 'http://assets.pokemon.com/assets/cms2/img/pokedex/full/017.png',
       'isMegaEvolution': false
-    };
-    let arbok = {
+    },
+    {
       'id': 4,
       'name': 'arbok',
       'pokemon_number': 30,
@@ -59,8 +63,8 @@ describe('API Routes', () => {
       'ability_1': 'intimidate',
       'url_image': 'http://assets.pokemon.com/assets/cms2/img/pokedex/full/024.png',
       'isMegaEvolution': false
-    };
-    let pikachu = {
+    },
+    {
       'id': 5,
       'name': 'pikachu',
       'pokemon_number': 31,
@@ -68,8 +72,8 @@ describe('API Routes', () => {
       'ability_1': 'static',
       'url_image': 'http://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png',
       'isMegaEvolution': false
-    };
-    let clefairy = {
+    },
+    {
       'id': 6,
       'name': 'clefairy',
       'pokemon_number': 41,
@@ -77,8 +81,8 @@ describe('API Routes', () => {
       'ability_1': 'cute-charm',
       'url_image': 'http://assets.pokemon.com/assets/cms2/img/pokedex/full/035.png',
       'isMegaEvolution': false
-    };
-    let jigglypuff = {
+    },
+    {
       'id': 7,
       'name': 'jigglypuff',
       'pokemon_number': 45,
@@ -86,8 +90,8 @@ describe('API Routes', () => {
       'ability_1': 'cute-charm',
       'url_image': 'http://assets.pokemon.com/assets/cms2/img/pokedex/full/039.png',
       'isMegaEvolution': false
-    };
-    let charmeleon = {
+    },
+    {
       'id': 8,
       'name': 'charmeleon',
       'pokemon_number': 6,
@@ -95,30 +99,58 @@ describe('API Routes', () => {
       'ability_1': 'blaze',
       'url_image': 'http://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png',
       'isMegaEvolution': false
-    };
+    }];
+    */
+
+  let Raichu = {
+    id: expect.any(Number),
+    name: 'Raichuchu',
+    pokemon_number: 32,
+    type_1: 'electric',
+    ability_1: 'static',
+    url_image: '',
+    isMegaEvolution: false
+  };
+
+
+  it('POST tramp to /api/pokemon', async () => {
+    const response = await request
+      .post('/api/dogs')
+      .send(Raichu);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(Raichu);
+
+    Raichu = response.body;
   });
+
 
   // If a GET request is made to /api/pokemon, does:
   // 1) the server respond with status of 200
   // 2) the body match the expected API data?
-  it.skip('GET /api/pokemon', async () => {
-    // act - make the request
-    const response = await request.get('/api/pokemon');
+  //it('GET /api/pokemon', async () => {
+  // act - make the request
+  //const response = await request.get('/api/pokemon');
 
-    // was response OK (200)?
-    expect(response.status).toBe(200);
+  // was response OK (200)?
+  //expect(response.status).toBe(200);
 
-    // did it return the data we expected?
-    expect(response.body).toEqual(expectedPokemon);
+  // did it return the data we expected?
+  // eslint-disable-next-line no-undef
+  //expect(response.body).toEqual(expectedPokemon);
 
-  });
+  //});
 
   // If a GET request is made to /api/pokemon/:id, does:
   // 1) the server respond with status of 200
   // 2) the body match the expected API data for the cat with that id?
+
+  /*
   it.skip('GET /api/pokemon/:id', async () => {
     const response = await request.get('/api/pokemon/2');
     expect(response.status).toBe(200);
     expect(response.body).toEqual(expectedPokemon[1]);
   });
+});
+*/
 });
